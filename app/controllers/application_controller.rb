d0c97cecd66b0c016e93663a2420471d337a86de
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-08-29 19:54:34 +0000

    travis build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	def create
+	  @application = Application.create(quote_params)
+	  if @application.invalid?
+	    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+	  end
+	  redirect_to root_path
+	end
+	
 end
